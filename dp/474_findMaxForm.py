5db6a73229ac65d3da51976594ffdbcecb346762
HA-Video-Bench <1310091179@qq.com>
2025-03-19 00:03:25 +0800

    dp 1 part1

---
+++ dp/474_findMaxForm.py
@@ -0,0 +1,3 @@
+class Solution:
+    def findMaxForm(self, strs: list[str], m: int, n: int) -> int:
+        
--- dp/494_findTargetSumWays.py
+++ dp/494_findTargetSumWays.py
@@ -1,24 +1,26 @@
 class Solution:
     def findTargetSumWays(self, nums: list[int], target: int) -> int:
         sum_ = sum(nums)
-        dp = [[0]*(2*sum_+1) for _ in range(len(nums))]
         if sum_ < abs(target):
             return 0
-        #init dp
-        dp[0][nums[0]+sum_] = 1
-        dp[0][-nums[0]+sum_] += 1
-        for i in range(len(nums)):
-            dp[i][2*sum_] = 1
-            dp[i][0] = 1
-            
+        
+        dp = [[0] * (2 * sum_ + 1) for _ in range(len(nums))]
+
+        # 初始化 dp
+        dp[0][nums[0] + sum_] = 1
+        dp[0][-nums[0] + sum_] += 1
+        # 递推
         for i in range(1, len(nums)):
-            for j in range(2*sum_+1):
+            for j in range(2 * sum_ + 1):
                 jj = j-sum_
-                if jj - nums[i] > -sum_:
+                if jj - nums[i] >= -sum_:
                     dp[i][j] += dp[i-1][j-nums[i]]
-                if jj + nums[i] < sum_:
+                if jj + nums[i] <= sum_:
                     dp[i][j] += dp[i-1][j+nums[i]]
+
         return dp[-1][target+sum_]
+
+
 
 if __name__ == '__main__':
     nums = [1, 1, 1, 1, 1]
@@ -31,3 +33,9 @@ if __name__ == '__main__':
     nums = [1]
     target = 2
     print(solution.findTargetSumWays(nums, target)) # 0
+    nums = [0,0,0,0,0,0,0,0,1]
+    target = 1
+    print(solution.findTargetSumWays(nums, target)) # 256
+    nums = [1,0]
+    target = 1
+    print(solution.findTargetSumWays(nums, target)) # 1
