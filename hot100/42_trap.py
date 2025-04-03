be5e9f12a45733da291e40043de2c3efcf832bcc
Chenzhou2344 <siyuan-l@sjtu.edu.cn>
2025-04-03 19:01:48 +0800

    hot 100 drop

--- hot100/42_trap.py
+++ hot100/42_trap.py
@@ -1,7 +1,11 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        pre = 0
-        post = 2
-        n = len(height)
-    
-        while post<n:
+
+
+if __name__ == "__main__":
+    s = Solution()
+    # print(s.trap([0,1,0,2,1,0,1,3,2,1,2,4]))  # Output: 6
+    print(s.trap([4,2,0,3,2,5]))  # Output: 9
+    print(s.trap([1]))  # Output: 0
+    print(s.trap([5,4,1,2]))  # Output: 1
+
